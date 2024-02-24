a09523401aef642e337224ba13e44019fc97b89d
Yannick Alexander <yannick@alexanderdev.io>
2024-02-24 11:51:58 +0100

    removed some basic boilderplate code

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to Tauri! 2</h1>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    </div>
+  );
+}
+
+export default Home;
